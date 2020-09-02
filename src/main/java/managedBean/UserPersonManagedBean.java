016f9eec725b7233b97ac71c0e8be7d1bb227ffc
teste <edercribeiro@gmail.com>
2020-09-01 22:32:14 -0300

    Consuming Web Service Postal Code RESTFUl on JSF

--- src/main/java/managedBean/UserPersonManagedBean.java
+++ src/main/java/managedBean/UserPersonManagedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
 
 import dao.DaoUser;
 import model.UserPerson;
@@ -19,11 +27,49 @@ public class UserPersonManagedBean {
 	private UserPerson userPerson = new UserPerson();
 	private List<UserPerson> list = new ArrayList<UserPerson>();
 	private DaoUser<UserPerson> daoGeneric = new DaoUser<UserPerson>();
-	
-	/*After the Bean were constructed in the memmory this method will be executed*/
+
+	/*
+	 * After the Bean were constructed in the memmory this method will be executed
+	 */
 	@PostConstruct
 	public void init() {
 		list = daoGeneric.list(UserPerson.class);
+	}
+
+	public void findCep(AjaxBehaviorEvent event) {
+		try {
+			
+			URL url = new URL("https://viacep.com.br/ws/"+ userPerson.getCep() +"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+			}
+			
+			UserPerson userCepPerson = new Gson().
+					fromJson(jsonCep.toString(), UserPerson.class);
+			
+			userPerson.setCep(userCepPerson.getCep());
+			userPerson.setLogradouro(userCepPerson.getLogradouro());
+			userPerson.setComplemento(userCepPerson.getComplemento());
+			userPerson.setBairro(userCepPerson.getBairro());
+			userPerson.setLocalidade(userCepPerson.getLocalidade());
+			userPerson.setUf(userCepPerson.getUf());
+			userPerson.setIbge(userCepPerson.getIbge());
+			userPerson.setGia(userCepPerson.getGia());
+			userPerson.setDdd(userCepPerson.getDdd());
+			userPerson.setSiafi(userCepPerson.getSiafi());
+			
+			System.out.println(userPerson);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public UserPerson getUserPerson() {
@@ -39,18 +85,18 @@ public class UserPersonManagedBean {
 		list.add(userPerson);
 		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Information: ", "Save Successfully!"));
-		return ""; /*If returns Null stays on the same screen*/
+		return ""; /* If returns Null stays on the same screen */
 	}
-	
+
 	public String newPerson() {
-		userPerson = new UserPerson();/*Erase the screen*/
+		userPerson = new UserPerson();/* Erase the screen */
 		return "";
 	}
-	
+
 	public List<UserPerson> getList() {
 		return list;
 	}
-	
+
 	public String delete() {
 		try {
 			daoGeneric.deleteUser(userPerson);
@@ -60,15 +106,13 @@ public class UserPersonManagedBean {
 					new FacesMessage(FacesMessage.SEVERITY_INFO, "Information: ", "Removed Successfully!"));
 		} catch (Exception e) {
 			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-				FacesContext.getCurrentInstance().addMessage(null,
-						new FacesMessage(FacesMessage.SEVERITY_INFO,
-								"Information: ", "Cannot delete - Exist phone number for the user!"));
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Information: ", "Cannot delete - Exist phone number for the user!"));
 			} else {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
 
 		return "";
 	}
--- src/main/java/model/UserPerson.java
+++ src/main/java/model/UserPerson.java
@@ -12,10 +12,8 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 @Entity
-@NamedQueries({
-	@NamedQuery(name = "UserPerson.findAll", query = "select u from UserPerson u"),
-	@NamedQuery(name = "UserPerson.findByName", query = "select u from UserPerson u where u.name = :name")
-})
+@NamedQueries({ @NamedQuery(name = "UserPerson.findAll", query = "select u from UserPerson u"),
+		@NamedQuery(name = "UserPerson.findByName", query = "select u from UserPerson u where u.name = :name") })
 public class UserPerson {
 
 	@Id
@@ -29,22 +27,33 @@ public class UserPerson {
 	private String password;
 	private Integer age;
 	private String sex;
-	
+
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String ibge;
+	private String gia;
+	private String ddd;
+	private String siafi;
+
 	@OneToMany(mappedBy = "userPerson", fetch = FetchType.EAGER)
 	private List<TelephoneUser> telephoneUsers;
-	
+
 	public String getSex() {
 		return sex;
 	}
-	
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
+
 	public Integer getAge() {
 		return age;
 	}
-	
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
@@ -96,20 +105,102 @@ public class UserPerson {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public List<TelephoneUser> getTelephoneUsers() {
 		return telephoneUsers;
 	}
-	
+
 	public void setTelephoneUsers(List<TelephoneUser> telephoneUsers) {
 		this.telephoneUsers = telephoneUsers;
 	}
 
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getGia() {
+		return gia;
+	}
+
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getSiafi() {
+		return siafi;
+	}
+
+	public void setSiafi(String siafi) {
+		this.siafi = siafi;
+	}
+
 	@Override
 	public String toString() {
-		return "UserPerson [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email
-				+ ", login=" + login + ", password=" + password + ", age=" + age + ", sex=" + sex + ", telephoneUsers=" + telephoneUsers
-				+ "]";
+		return "UserPerson [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email + ", login="
+				+ login + ", password=" + password + ", age=" + age + ", sex=" + sex + ", cep=" + cep + ", logradouro="
+				+ logradouro + ", complemento=" + complemento + ", bairro=" + bairro + ", localidade=" + localidade
+				+ ", uf=" + uf + ", ibge=" + ibge + ", gia=" + gia + ", ddd=" + ddd + ", siafi=" + siafi
+				+ ", telephoneUsers=" + telephoneUsers + "]";
 	}
 
 	@Override
@@ -136,9 +227,5 @@ public class UserPerson {
 			return false;
 		return true;
 	}
-
-	
-	
-	
 
 }
